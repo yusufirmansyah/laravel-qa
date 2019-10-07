295c04b57b75c14c3b6665d790fadeb45e2b9862
Yusuf Firman Syah <yusuf.firmansyah@gmail.com>
2019-10-07 23:18:59 +0300

    Implement authorization using Gates

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -36,8 +36,11 @@
                             <div class="d-flex align-center">
                                 <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                 <div class="ml-auto">
+                                    @if (Auth::user()->can('update-question', $question))
                                     <a href="{{ route('questions.edit', $question->id) }}"
                                         class="btn btn-sm btn-outline-info">Edit</a>
+                                    @endif
+                                    @if (Auth::user()->can('delete-question', $question))
                                     <form action="{{ route('questions.destroy', $question->id) }}" method="post"
                                         class="form-delete">
                                         @method('DELETE')
@@ -45,6 +48,7 @@
                                         <button type="submit" class="btn btn-sm btn-outline-danger"
                                             onclick="return confirm('Are you sure?')">Delete</button>
                                     </form>
+                                    @endif
                                 </div>
                             </div>
                             <p class="lead">
